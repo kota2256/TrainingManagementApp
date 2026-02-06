c54e242e3b2d8c9d1a7820bba6e24a26ffd9a7af
koto <koto@localhost>
2026-02-07 00:44:01 +0900

    260206_セットアップ

---
+++ TrainingManagementApp_tanaka/src/main/resources/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS roles(
+	code	INT UNIQUE NOT NULL,		--参照ID	1:管理者, 2:一般ユーザ
+	"value"	VARCHAR(255) NOT NULL		--権限	管理者:ADMIN,  一般ユーザ:GENERAL
+);
+
+CREATE TABLE IF NOT EXISTS users(
+	id					INT AUTO_INCREMENT,
+	role_code	INT,
+	name			VARCHAR(255) NOT NULL,
+	password	VARCHAR(60) NOT NULL,
+	email			VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id), --プライマリーキー
+	FOREIGN KEY(role_code)	REFERENCES roles(code)	--外部キー
+);
+
+CREATE TABLE IF NOT EXISTS physical_details(
+user_id				INT NOT NULL,
+created_at			TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,	--年月日時分秒（小数部なし）、値を指定しなければ現在時刻が自動挿入
+weight					DECIMAL(4, 1) NOT NULL,	--DECIMAL(5,2)：整数部分5桁まで、小数部分2桁
+recorded_date	DATE NOT NULL,
+PRIMARY KEY(user_id, created_at), --プライマリーキー
+FOREIGN KEY(user_id) REFERENCES users(id)		--外部キー
+);
+
