e73b840e33ec6a69317f8555ec3446470becca5b
koto <koto@localhost>
2026-02-07 09:47:48 +0900

    260207_桁数指定（CHECK制約）

--- TrainingManagementApp_tanaka/src/main/resources/schema.sql
+++ TrainingManagementApp_tanaka/src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS roles(
 	code	INT UNIQUE NOT NULL,		--参照ID	1:管理者, 2:一般ユーザ
-	"value"	VARCHAR(255) NOT NULL		--権限	管理者:ADMIN,  一般ユーザ:GENERAL
+	"value"	VARCHAR(255) NOT NULL,		--権限	管理者:ADMIN,  一般ユーザ:GENERAL
+	CHECK (code <= 999)
 );
 
 CREATE TABLE IF NOT EXISTS users(
@@ -10,7 +11,9 @@ CREATE TABLE IF NOT EXISTS users(
 	password	VARCHAR(60) NOT NULL,
 	email			VARCHAR(255) NOT NULL,
 	PRIMARY KEY(id), --プライマリーキー
-	FOREIGN KEY(role_code)	REFERENCES roles(code)	--外部キー
+	FOREIGN KEY(role_code)	REFERENCES roles(code),	--外部キー
+	CHECK (id <= 999),
+	CHECK (role_code <= 999)
 );
 
 CREATE TABLE IF NOT EXISTS physical_details(
@@ -19,6 +22,7 @@ created_at			TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,	--年月日時分秒（小�
 weight					DECIMAL(4, 1) NOT NULL,	--DECIMAL(5,2)：整数部分5桁まで、小数部分2桁
 recorded_date	DATE NOT NULL,
 PRIMARY KEY(user_id, created_at), --プライマリーキー
-FOREIGN KEY(user_id) REFERENCES users(id)		--外部キー
+FOREIGN KEY(user_id) REFERENCES users(id),		--外部キー
+CHECK (user_id <= 999)
 );
 
